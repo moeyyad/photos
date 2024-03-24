ffc982b672e34c372df3292d1f3105269a3a27eb
Moeyyad Qureshi <moeyyadqureshi@gmail.com>
2024-03-24 15:56:34 -0400

    deploying to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://moeyyad-showcase.surge.sh',
+  site: 'https://moeyyad.github.io',
+  base: 'photos',
 })
